eb0ca97a5d7dfcde62ae718f876fd98779da72cd
workplace <31040709+Rhythmoo@users.noreply.github.com>
2018-07-08 10:04:15 +0800

    Add files via upload

---
+++ web-ssd.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  5 20:02:09 2018
+
+@author: oliver
+"""
+
+import os
+from flask import Flask, request, url_for, send_from_directory
+from werkzeug import secure_filename
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+import math
+import random
+
+import numpy as np
+import tensorflow as tf
+import cv2
+
+slim = tf.contrib.slim
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import sys
+sys.path.append('../')
+
+from nets import ssd_vgg_300, ssd_common, np_methods
+from preprocessing import ssd_vgg_preprocessing
+from notebooks import visualization
+
+gpu_options = tf.GPUOptions(allow_growth=True)
+config = tf.ConfigProto(log_device_placement=False, gpu_options=gpu_options)
+isess = tf.InteractiveSession(config=config)
+
+# Input placeholder.
+net_shape = (300, 300)
+data_format = 'NHWC'
+img_input = tf.placeholder(tf.uint8, shape=(None, None, 3))
+# Evaluation pre-processing: resize to SSD net shape.
+image_pre, labels_pre, bboxes_pre, bbox_img = ssd_vgg_preprocessing.preprocess_for_eval(
+    img_input, None, None, net_shape, data_format, resize=ssd_vgg_preprocessing.Resize.WARP_RESIZE)
+image_4d = tf.expand_dims(image_pre, 0)
+
+# Define the SSD model.
+reuse = True if 'ssd_net' in locals() else None
+ssd_net = ssd_vgg_300.SSDNet()
+with slim.arg_scope(ssd_net.arg_scope(data_format=data_format)):
+    predictions, localisations, _, _ = ssd_net.net(image_4d, is_training=False, reuse=reuse)
+
+# Restore SSD model.
+ckpt_filename = '../checkpoints/model1.ckpt'
+# ckpt_filename = '../checkpoints/VGG_VOC0712_SSD_300x300_ft_iter_120000.ckpt'
+isess.run(tf.global_variables_initializer())
+saver = tf.train.Saver()
+saver.restore(isess, ckpt_filename)
+
+# SSD default anchor boxes.
+ssd_anchors = ssd_net.anchors(net_shape)
+
+def process_image(img, select_threshold=0.5, nms_threshold=.45, net_shape=(300, 300)):
+    # Run SSD network.
+    rimg, rpredictions, rlocalisations, rbbox_img = isess.run([image_4d, predictions, localisations, bbox_img],
+                                                              feed_dict={img_input: img})
+    
+    # Get classes and bboxes from the net outputs.
+    rclasses, rscores, rbboxes = np_methods.ssd_bboxes_select(
+            rpredictions, rlocalisations, ssd_anchors,
+            select_threshold=select_threshold, img_shape=net_shape, num_classes=21, decode=True)
+    
+    rbboxes = np_methods.bboxes_clip(rbbox_img, rbboxes)
+    rclasses, rscores, rbboxes = np_methods.bboxes_sort(rclasses, rscores, rbboxes, top_k=400)
+    rclasses, rscores, rbboxes = np_methods.bboxes_nms(rclasses, rscores, rbboxes, nms_threshold=nms_threshold)
+    # Resize bboxes to original image shape. Note: useless for Resize.WARP!
+    rbboxes = np_methods.bboxes_resize(rbbox_img, rbboxes)
+    return rclasses, rscores, rbboxes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = os.getcwd()
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+html = '''
+    <!DOCTYPE html>
+    <title>Upload File</title>
+    <h1>SSD</h1>
+    <form method=post enctype=multipart/form-data>
+         <input type=file name=file>
+         <input type=submit value=上传>
+    </form>
+    '''
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file_url = url_for('uploaded_file', filename=filename)
+            
+            img = mpimg.imread(filename)
+            rclasses, rscores, rbboxes =  process_image(img)
+            fig = visualization.plt_bboxes(img, rclasses, rscores, rbboxes)
+            new_filename = "%s.png"%(filename.split('.')[0]+'_result')
+            fig.savefig(new_filename)
+            new_file_url = url_for('uploaded_file',filename=new_filename)
+            #new_file_url = url_for('uploaded_file', filename='test.png')
+            return html + '<table><tr><td><img src=' + file_url + ' border=0></td> <td><img src=' + new_file_url + ' border=0></td> </tr></table>'
+    return html
+
+
+if __name__ == '__main__':
+    app.run()
